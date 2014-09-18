cf1265b5ac3fb12ce91c8767a1d7f560dc719053
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2014-09-18 14:18:41 +0200

    Add turer to data import/export scripts

--- utils/import_test_data.sh
+++ utils/import_test_data.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for col in steder bilder områder grupper; do
+for col in turer steder bilder områder grupper; do
   mongoimport --drop --jsonArray --db test --collection $col --file "test/data/$col.json"
 done
 
